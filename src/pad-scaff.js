96e617ed21c1f4eddf5f2233e2885b88bb71b7db
andrewtheiller37 <89604885+andrewtheiller37@users.noreply.github.com>
2021-10-25 17:53:25 -0400

    Update pad-scaff.js

--- src/pad-scaff.js
+++ src/pad-scaff.js
@@ -1,6 +1,10 @@
 import { html, css } from 'lit';
 import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 
+import './pad-header.js';
+import './pad-icon.js';
+import './pad-card.js'; //imported so they can be used in the scaffold
+
 export class padScaff extends LitElement {
     static get tag() {
     return 'pad-scaff';
@@ -8,5 +12,47 @@ export class padScaff extends LitElement {
 
 constructor() {
     super();
-
+    this.title = "Paddy's Hub";
 }
+
+static get properties() {
+    return {
+        ...super.properties,
+        title: { type: String },
+    };
+}
+
+static get styles() {
+    return [
+      ...super.styles,
+      css`
+        :host {
+            #content {
+                border-top: transparent;
+                background-color: white;
+                border: 1px solid black;
+                padding-top: 10px;
+                padding-bottom: 10px;
+                padding-left: 100px;
+                margin: 0px;
+            }
+        }
+      `,
+    ];
+  }
+
+render() {
+    return html `
+        <div id="card">
+            <slot name="card"></slot>
+        </div>
+        <div id="icon">
+            <slot name="icon"></slot>
+        </div>
+        <div id="header">
+            <slot name="header"></slot>
+        </div>
+    `;
+}
+
+window.customElements.define(padScaff.tag, padScaff);
