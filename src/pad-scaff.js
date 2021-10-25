88a135b94f57eb484bfbbd92a347869ad2f2ea53
andrewtheiller37 <89604885+andrewtheiller37@users.noreply.github.com>
2021-10-25 17:23:53 -0400

    Update pad-scaff.js

--- src/pad-scaff.js
+++ src/pad-scaff.js
@@ -1,1 +1,12 @@
+import { html, css } from 'lit';
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 
+export class padScaff extends LitElement {
+    static get tag() {
+    return 'pad-scaff';
+}
+
+constructor() {
+    super();
+
+}
