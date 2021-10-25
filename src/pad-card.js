8a8f19a59fe60b1e31ad12b0079b0268aba45024
andrewtheiller37 <89604885+andrewtheiller37@users.noreply.github.com>
2021-10-25 17:22:21 -0400

    Update pad-card.js

--- src/pad-card.js
+++ src/pad-card.js
@@ -1,1 +1,14 @@
+import { html, css } from 'lit';
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
+
+export class padCard extends LitElement {
+    static get tag() {
+    return 'pad-card';
+}
+
+constructor() {
+    super();
+
+}
+
 
