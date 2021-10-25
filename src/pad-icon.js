9b801ba46453e2f4ac51fc80ee968c42cbc317ed
andrewtheiller37 <89604885+andrewtheiller37@users.noreply.github.com>
2021-10-25 18:20:04 -0400

    Update pad-icon.js

--- src/pad-icon.js
+++ src/pad-icon.js
@@ -1,7 +1,7 @@
 import { html, css } from 'lit';
 import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 
-export class padIcon extends SimpleColors {
+export class padIcon extends SimpleColors { /* A lot of this can be taken from learning card, since it already contains the icons */
     static get tag() {
     return 'pad-icon';
 }
