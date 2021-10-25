357851bf84691cd72c402c84deed10f047153d62
andrewtheiller37 <89604885+andrewtheiller37@users.noreply.github.com>
2021-10-25 18:09:46 -0400

    Update pad-icon.js

--- src/pad-icon.js
+++ src/pad-icon.js
@@ -1,12 +1,35 @@
 import { html, css } from 'lit';
 import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 
-export class padIcon extends LitElement {
+export class padIcon extends SimpleColors {
     static get tag() {
     return 'pad-icon';
 }
 
 constructor() {
     super();
-    
+
 }
+
+static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+    };
+  }
+
+static get styles() {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html``;
+  }
+} 
